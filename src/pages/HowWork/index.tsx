6fcd405cbf88b9453d867950fcf5e7153f424022
Rubens Diniz <rubensadiniz@edu.unifor.br>
2022-08-25 11:09:01 -0300

    How it works page

--- src/pages/HowWork/index.tsx
+++ src/pages/HowWork/index.tsx
@@ -1,16 +1,19 @@
 import React from "react";
 import Layout from "../Layout";
-import { Card, Container } from "./styles";
+import { Card, Container, StepsContainer } from "./styles";
 
 const HowWork: React.FC = () => {
   return (
     <Layout>
       <Container>
         <Card>
-          <h1>How this Work</h1>
-          <span>...</span>
-          <span>...</span>
-          <span>...</span>
+          <h1>How it Works</h1>
+          <StepsContainer>
+            <span>1. Connect your Klever wallet to login to Kazu</span>
+            <span>2. After connecting, you'll get 3 free tickets to play with</span>
+            <span>3. Head to the Store tab to buy Kazu packs or Marketplace tab to buy Kazus from other players</span>
+            <span>4. Go to the Battle tab. You are now ready to battle Kazus!</span>
+          </StepsContainer>
         </Card>
       </Container>
     </Layout>
--- src/pages/HowWork/styles.ts
+++ src/pages/HowWork/styles.ts
@@ -10,10 +10,12 @@ export const Container = styled.div`
 
 export const Card = styled.div`
   padding: 2.5rem;
+  margin-top: 2rem;
 
   display: flex;
 
   flex-direction: column;
+  align-items: center;
 
   background: ${(props) => props.theme.blur.black};
   border-radius: 0.6875rem;
@@ -21,5 +23,19 @@ export const Card = styled.div`
 
   h1 {
     color: ${(props) => props.theme.white};
+    margin-bottom: 0.5rem;
+  }
+`;
+
+export const StepsContainer = styled.div`
+  display: flex;
+
+  flex-direction: column; 
+  max-width: 30rem;
+
+  span {
+    opacity: 0.7;
+    font-weight: 400;
+    margin-top: 1rem;
   }
 `;
