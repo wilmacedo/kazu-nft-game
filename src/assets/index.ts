49491b2074f2d27c191f1286dcbf51762142671b
saviojks <saviooribe@gmail.com>
2022-08-25 18:18:39 -0300

    randomNFt

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -2,5 +2,8 @@ import ArrowDown from "./arrow-down.svg";
 import KLVIcon from "./klv-icon.svg";
 import Whale from "./whale.png";
 import Kazu1 from "./kazu1.png";
+import Kazu2 from "./kazu2.png";
+import Kazu3 from "./kazu3.png";
+import Kazu4 from "./kazu4.png";
 
-export { ArrowDown, KLVIcon, Whale, Kazu1 };
+export { ArrowDown, KLVIcon, Whale, Kazu1,Kazu2,Kazu3,Kazu4 };
